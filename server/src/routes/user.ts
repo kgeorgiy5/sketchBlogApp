b16e484775ce0722a2c78289c617c8e0f5d37235
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-11-14 10:54:56 +0200

    Fix: Fixed controller imports

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 
-import * as userControllers from "../controllers/user";
+import * as userController from "../controllers/user";
 
 const router = Router();
 
-router.get("/my-posts", userControllers.getUserPosts);
+router.get("/my-posts", userController.getUserPosts);
 
-router.post("/create-post", userControllers.postCreatePost);
+router.post("/create-post", userController.postCreatePost);
 
-router.put("/edit-post", userControllers.putUpdatePost);
-router.put("/like-post", userControllers.putLikePost);
+router.put("/edit-post", userController.putUpdatePost);
+router.put("/like-post", userController.putLikePost);
 
 
 export default router;
