bbc967314a0e626f4b346e8681bfd3985d1d5c2b
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-11-16 00:06:55 +0200

    Feat: added redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-// import { createStoreHook, Provider } from "react-redux";
-
-// const store = createStoreHook();
-{/* <Provider store={store}> */ }
-{/*   <App /> */ }
-{/* </Provider> */ }
+import { Provider } from 'react-redux'
+import store from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
 
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
