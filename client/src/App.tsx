451ef6b156960c8226e9f3c888a055391ef9cc26
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-11-24 13:06:13 +0200

    Feat: added post details page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import Sketch from './components/sketch/Sketch';
 import useCheckAuth from "./hooks/auth/useCheckAuth.ts";
 import ProtectedRoute from "./components/auth/ProtectedRoute.tsx";
 import ErrorStack from "./components/ErrorStack.tsx";
+import PostDetails from "./components/postDetails/PostDetails.tsx";
 
 function App() {
     useCheckAuth();
@@ -17,6 +18,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element=<Feed/> />
+          <Route path="/post/:id" element={<PostDetails/>} />
           <Route element={<ProtectedRoute/>}>
               <Route path="/my-posts" element=<MyPosts/> />
               <Route path="/sketch" element=<Sketch/> />
