51ec9eccba03e2c8f454a2e7586424881e331f39
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-11-24 18:50:59 +0200

    .env deployment hotfix

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,11 +8,14 @@ import routes from "./routes/routes";
 import errorMiddleware from "./middleware/errorMiddleware";
 import cors from "cors";
 
-config({ path: ".env", example: ".env" });
 const PORT = process.env.PORT;
 const MONGODB_URI: string = process.env.MONGODB_URI as string;
 const SESSION_SECRET: string = process.env.SESSION_SECRET as string;
 const ORIGIN: string = process.env.ORIGIN as string;
+
+if (process.env.NODE_ENV !== 'production') {
+  config({ path: ".env", example: ".env" });
+}
 
 const MongoDbStore = connectMongoDbSession(session);
 
