a2465aba6a0940278a281c3a2af74d689d7f5b28
kgeorgiy5 <138616643+kgeorgiy5@users.noreply.github.com>
2024-11-24 22:08:02 +0200

    hotfix3

--- server/src/app.ts
+++ server/src/app.ts
@@ -45,7 +45,7 @@ app.use(
     saveUninitialized: false,
     resave: false,
       cookie:{
-        httpOnly:false,
+        httpOnly:true,
           secure:true,
           sameSite:"none",
           maxAge:3600000,
